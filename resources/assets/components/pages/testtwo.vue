130355823218a8ab504274ad4a06d00640759d81
BuildTools <unconfigured@null.spigotmc.org>
2018-05-08 12:31:41 +1000

    First commit

---
+++ resources/assets/components/pages/testtwo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="sample-page">
+            <div class="container-fluid bg-white">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>Hello World</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+<!-- styles -->
+<!-- adding scoped attribute will apply the css to this component only -->
+<style scoped>
+
+</style>
